c1454664762d283d4f605372bab5cf61aab6e0ad
manishadtanii <manishadtani99@gmail.com>
2025-12-24 15:18:08 +0530

    changes done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['https://552a721652e2.ngrok-free.app','http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: '*',
   // credentials: true,
   optionsSuccessStatus: 200,
 };
